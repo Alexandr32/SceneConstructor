ecad78d40d7c09f343d53e3051292912d6cda489
Alex <alex3289.aa@gmai.com>
2021-08-23 14:00:36 +0500

    Рабочее пространство

--- scene-constructor/src/app/app.module.ts
+++ scene-constructor/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      EditorComponent
    ],
   imports: [
     BrowserModule,
