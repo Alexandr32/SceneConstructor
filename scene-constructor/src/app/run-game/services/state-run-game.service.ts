1cd10d7d8dffd7ff7242d1c5d4a9b6c6c2e5cd90
Alex <alex3289.aa@gmai.com>
2021-12-26 15:42:44 +0500

    Логика

--- scene-constructor/src/app/run-game/services/state-run-game.service.ts
+++ scene-constructor/src/app/run-game/services/state-run-game.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {IBaseSceneRunGame} from "../models/other-models/base-scene-run-game.model";
 import {RunGame} from "../models/other-models/run-game.model";
 import {RunGameService} from "./run-game.service";
 import {Observable, of, Subject} from "rxjs";
 import {StateService} from "./state.service";
 import {Player} from "../models/other-models/player.model";
+import {TypeSceneEnum} from "../../core/models/type-scene.enum";
+import {StateGame} from "../models/other-models/state-game.model";
 
 @Injectable({
   providedIn: 'root'
@@ -41,49 +43,59 @@ export class StateRunGameService {
         this.currentScene$.next(currentScene)
       }
 
-      // Кто должен отвечать
-     const players = currentScene.players
-
-      // Кол-во ожидаемых ответов
-      const countAnswer = players.length
-      const currentAnswer = x.answer
-
-      if(!currentAnswer) {
-        return;
+      if(currentScene.typesScene === TypeSceneEnum.Answer) {
+        this.selectAnswer(currentScene, x)
       }
 
-      if(countAnswer !== currentAnswer.length) {
-        return
-      }
-
-      const map = new Map<string, number>()
-
-      currentAnswer.forEach(item => {
-        let hasAnswer = map.has(item.answerId)
-        if(hasAnswer) {
-          let value = map.get(item.answerId)
-          map.set(item.answerId, value + 1)
-        } else {
-          map.set(item.answerId, 1)
-        }
-      })
-
-      //  Выбранный ответ
-      const selectAnswer =  [...map.entries()]?.reduce((a, e ) => e[1] > a[1] ? e : a)
-
-      if(selectAnswer) {
-        console.log('Ответы не найдены')
-      }
-
-      // Список доступных ответов
-      const answers = currentScene.answers.find(a => a.id === selectAnswer[0])
-      const scene = this.scenesMap.get(answers.sceneId)
-
-      this.stateService.nextDataStateGame(x.id, scene).then((error) => {
-        console.error('При смене сцены произошла ошибка:', error)
-      })
     })
   }
+
+  private selectAnswer(currentScene: IBaseSceneRunGame, stateGame: StateGame) {
+
+    // Кто должен отвечать
+    const players = currentScene.players
+
+    // Кол-во ожидаемых ответов
+    const countAnswer = players.length
+    const currentAnswer = stateGame.answer
+
+    if(!currentAnswer) {
+      return;
+    }
+
+    if(countAnswer !== currentAnswer.length) {
+      return
+    }
+
+
+    const map = new Map<string, number>()
+
+    currentAnswer.forEach(item => {
+      let hasAnswer = map.has(item.answerId)
+      if(hasAnswer) {
+        let value = map.get(item.answerId)
+        map.set(item.answerId, value + 1)
+      } else {
+        map.set(item.answerId, 1)
+      }
+    })
+
+    //  Выбранный ответ
+    const selectAnswer =  [...map.entries()]?.reduce((a, e ) => e[1] > a[1] ? e : a)
+
+    if(selectAnswer) {
+      console.log('Ответы не найдены')
+    }
+
+    // Список доступных ответов
+    const answers = currentScene.answers.find(a => a.id === selectAnswer[0])
+    const scene = this.scenesMap.get(answers.sceneId)
+
+    this.stateService.nextDataStateGame(stateGame.id, scene).then((error) => {
+      console.error('При смене сцены произошла ошибка:', error)
+    })
+  }
+
 
   selectedScene(selectSceneId: string) {
     const scene = this.scenesMap.get(selectSceneId)
